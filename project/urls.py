bff8c2f4d4bd9252339e110eb17606129196256a
Medet <m.shamuratov@gmail.com>
2023-10-10 15:28:07 +0600

    Initial commit

---
+++ project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='home'),
+    path('paypal/', PaypalView.as_view(), name='paypal'),
+    path('bank/', BankView.as_view(), name='bank'),
+    path('terminal/', Terminal.as_view(), name='terminal'),
+    path('banksort/', BankSort.as_view(), name='banksort'),   
+]
